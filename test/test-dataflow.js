cb170eb885de7eaaccff1a53a0fbbd796a60fb67
Patrick Dubroy <pdubroy@gmail.com>
2016-04-29 17:04:28 +0200

    Add a few more tests.

--- test/test-dataflow.js
+++ test/test-dataflow.js
@@ -3,16 +3,62 @@
 var Graph = require('..');
 var test = require('tape');
 
-test('basic', function(t) {
+test('simple', function(t) {
   var g = new Graph();
   g.define('a', [], 3);
-  g.define('b', ['a'], function(a) {
-    return a + 1;
-  });
-  t.equal(g.get('b'), 4);
+  g.define('b', ['a'], a => a + 1);
+  t.equal(g.get('b'), 4, 'variable has a value after define()');
   g.set('a', 10);
   t.equal(g.get('a'), 10);
-  t.equal(g.get('b'), 11);
+  t.equal(g.get('b'), 11, 'dependency updated after set()');
+
+  g.define('c', ['b'], b => b + 100);
+  t.equal(g.get('c'), 111);
+  g.set('a', 98);
+  t.equal(g.get('c'), 199, 'transitive dependency');
+
+  t.end();
+});
+
+test('multiple depedencies', function(t) {
+  var g = new Graph();
+  var updateCount = 0;
+
+  g.define('a', [], 5);
+  g.define('b', [], 7);
+  g.define('c', ['a', 'b'], (a, b) => {
+    updateCount++;
+    return a + b;
+  });
+  t.equal(g.get('c'), 12);
+  t.equal(updateCount, 1);
+
+  g.set('a', 0);
+  t.equal(g.get('c'), 7);
+  t.equal(updateCount, 2);
+
+  g.set('b', 1);
+  t.equal(g.get('c'), 1);
+  t.equal(updateCount, 3);
+
+  t.end();
+});
+
+test('joined dependencies', function(t) {
+  var g = new Graph();
+  var updateCount = 0;
+
+  g.define('a', [], 1);
+  g.define('b', ['a'], a => a + 1);
+  g.define('c', ['a', 'b'], (a, b) => {
+    updateCount++;
+    return a + b;
+  });
+  t.equal(g.get('c'), 3);
+  t.equal(updateCount, 1);
+  g.set('a', 5);
+  t.equal(g.get('c'), 11);
+  t.equal(updateCount, 2, 'only one update happens');
 
   t.end();
 });
